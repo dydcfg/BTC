140434b67f660581aa9a0e819ca1e793caee3c83
dydcfg <dydcfg@gmail.com>
2015-09-30 17:11:50 +0800

    Add Data Track Function

--- huobi/DataTracker.py
+++ huobi/DataTracker.py
@@ -5,9 +5,23 @@ import time
 import Util
 from Util import *
 import requests
+import datetime
+import json
 
 class DataTracker:
-    def setParams(self, interval):
-        resp = requests.get(HUOBI_TICKER_BTC)
-        resp = resp.json()
-        print resp
+    def track(self, interval):
+        flag = ""
+        while True:
+            myTime = datetime.datetime.now()
+            resp = requests.get(HUOBI_TICKER_BTC)
+            resp = resp.json()
+            print resp["trades"][0]
+            if flag != resp["trades"][0]:
+                filename = str(myTime.year)+str(myTime.month)+str(myTime.day)+".data"
+                print filename
+                fp = open(filename,'a+')
+                fp.write(json.dumps(resp)+"\n")
+                fp.close()
+                flag = resp["trades"][0]
+            time.sleep(interval)
+
